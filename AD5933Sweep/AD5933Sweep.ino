e87cdcddb8be1b74d369adb4928104d277dc451e
Tina‘s ROG Desktop <32335780+TingliangZhang@users.noreply.github.com>
2022-04-04 22:43:32 +0800

    Update AD5933Sweep.ino

--- AD5933Sweep/AD5933Sweep.ino
+++ AD5933Sweep/AD5933Sweep.ino
@@ -14,8 +14,69 @@ ad5933-test
 double gain[NUM_INCR+1];
 int phase[NUM_INCR+1];
 
+// 定义MUX的引脚
+int MUX[]={16,17,18,25,26,27};
+
+int * Decode83(int I){
+  int R[]={0,0,0};
+  int R0=0;
+  int R1=0;
+  int R2=0;
+  switch (I){
+    case 0:{R0=0;R1=0;R2=0;}
+    case 1:{R0=0;R1=0;R2=1;}
+    case 2:{R0=0;R1=1;R2=0;}
+    case 3:{R0=0;R1=1;R2=1;}
+    case 4:{R0=1;R1=0;R2=0;}
+    case 5:{R0=1;R1=0;R2=1;}
+    case 6:{R0=1;R1=1;R2=0;}
+    case 7:{R0=1;R1=1;R2=1;}
+    default:printf("error\n");
+  }
+  R[0]=R0;
+  R[1]=R1;
+  R[2]=R2;
+  return R;
+  }
+
+void SetPort1(int P){
+  int *R;
+  R=Decode83(P);
+  for (int i = 0; i < 3; i++ ){
+    digitalWrite(MUX[i], R[i]);
+  }
+  }
+
+void SetPort2(int P){
+  int *R;
+  R=Decode83(P);
+  for (int i = 0; i < 3; i++ ){
+    digitalWrite(MUX[i+3], R[i]);
+  }
+  }
+
+void ClearPins(){
+  for (int i = 0; i < 6; i++ ){
+    digitalWrite(MUX[i], LOW);
+  }
+  }
+
+void Link(int A, int B){
+  SetPort1(A);
+  SetPort2(B);
+}
+
+
+
+
+
 void setup(void)
 {
+  // Set MUX Output 
+  for (int i = 0; i < 6; i++ ){
+    pinMode(MUX[i], OUTPUT);
+  }
+  
   // Begin I2C
   Wire.begin();
 
